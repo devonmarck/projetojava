29beb97ce589c48dbcb80180e1c2df8ae314e127
OTAVIO LUIZ MARTINS DUARTE <otavio.lmduarte@senacsp.edu.br>
2024-01-19 09:10:15 -0300

    Estudos variaveis, operações aritméticas e atribuições, operadores relacionais e lógicos. uso de estrutura de decisão

---
+++ fundamentosJava/src/LgicaDeProgramaçao/OperadoresAritimeticos.java
@@ -0,0 +1,17 @@
+package LgicaDeProgramaçao;
+
+
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		int numero = 10;
+		System.out.println("Número = " + numero);
+		System.out.println("Adição = " + numero + " + 5 | Número = " + (numero + 5));
+		System.out.println("Subtração = " + numero + " - 5 | Número = " + (numero - 5));
+		System.out.println("Multiplicação = " + numero + " * 5 | Número = " + (numero * 5));
+		System.out.println("Divisão = " + numero + " / 5 | Número = " + (numero / 5));
+		System.out.println("Resto = " + numero + " % 5 | Número = " + (numero % 5));
+	}
+
+}
